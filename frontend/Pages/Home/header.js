3316803848230a8fb1d89bc5b72b673733706131
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-09-05 17:07:25 +0530

    Update Home.html, header.js, home.css, and home.js for special header functionality

--- frontend/Pages/Home/header.js
+++ frontend/Pages/Home/header.js
@@ -14,7 +14,14 @@ class SpecialHeader extends HTMLElement {
 
                     <div class="header-user">
                         <img id="cart" src="../../assets/cart.png">
-                        <img id="user" src="../../assets/user.png">
+                        <img id="user" src="../../assets/user.png" onClick="togglemenu()">
+                            <ul id="drop-down">
+                                <div class="box"></div>
+                                <li id="login-link"><a href="/login.html">Login</a></li>
+                                <li id="signup-link"><a href="/signup.html">Sign Up</a></li>
+                                <li id="profile-link" style="display:none;"><a href="/myProfile.html">My Profile</a></li>
+                                <li id="logout-link" style="display:none;"><a href="#" onclick="logout()">Logout</a></li>
+                            </ul>
                     </div>
                 </div>
             </header>
