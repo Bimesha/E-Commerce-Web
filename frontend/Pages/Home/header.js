9c9c270d9458732c5fb57f317d3b2ca02e10b8a1
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-09-05 20:27:38 +0530

    Update header links and styling for special header in Home page

--- frontend/Pages/Home/header.js
+++ frontend/Pages/Home/header.js
@@ -34,11 +34,9 @@ class SpecialHeader extends HTMLElement {
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Shop</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Separated link</a></li>
+                                <li><a class="dropdown-item" href="#">Chair Collection</a></li>
+                                <li><a class="dropdown-item" href="#">Table Collection</a></li>
+                                <li><a class="dropdown-item" href="#">Sofa Collection</a></li>
                             </ul>
                     </li>
                     <li class="nav-item">
