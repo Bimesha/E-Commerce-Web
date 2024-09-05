ec1c6453f1827791fd67d97295aa343b49bf50f3
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-09-05 17:16:02 +0530

    Add togglemenu function for dropdown menu in home.js

--- frontend/Pages/Home/home.js
+++ frontend/Pages/Home/home.js
@@ -1,1 +1,17 @@
-
+function togglemenu() {
+    let dropdown = document.getElementById("drop-down");
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+      dropdown.style.display = "block";
+    } else {
+      dropdown.style.display = "none";
+    }
+  }
+  
+  // Close the drop-down menu if the user clicks outside of it
+  window.onclick = function (event) {
+    var dropdown = document.getElementById("drop-down");
+    var avatar = document.getElementById("user");
+    if (!avatar.contains(event.target) && !dropdown.contains(event.target)) {
+      dropdown.style.display = "none";
+    }
+  };
