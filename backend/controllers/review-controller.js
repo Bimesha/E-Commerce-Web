0a6801c3db38607303ff55f6d213127a44aa831a
pawani mekhala <pawanimekhala3@gmail.com>
2024-10-14 13:55:24 +0530

    feat: add function to add a new review to review-controller

--- backend/controllers/review-controller.js
+++ backend/controllers/review-controller.js
@@ -1,10 +1,20 @@
 //handle adding reviews
 const Review = require('../models/review-model');
 
+//add a new review
 exports.addReview = (req, res) => {
-    const {UserID,Comment} = req.body;
+    const {userId,comment} = req.body;
 
-    if (!UserID || !Comment) {
-        return res.status(400).json({ error: 'User ID and comment are required' });
+    if (!comment || comment.trim() === '') {
+        return res.status(400).json({ error: 'Review comment cannot be empty.' });
     }
+
+    Review.addReview(userId, comment, (err, result) => {
+        if (err) {
+          return res.status(500).json({ error: 'Failed to add review' });
+        }
+        res.status(201).json({ message: 'Review added successfully' });
+    });    
 }
+
+//get all 
