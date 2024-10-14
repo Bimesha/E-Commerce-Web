cd4d5a6520b78c832c661aa68fdb032fa50f50db
pawani mekhala <pawanimekhala3@gmail.com>
2024-10-14 08:46:37 +0530

    feat: add getReviewsByUserId function to review model

--- backend/models/review-model.js
+++ backend/models/review-model.js
@@ -1,12 +1,22 @@
 const db = require('../config/db');
 
 // review model 
-const review = {
+const Review = {
     addReview: (UserID,Comment, callback) => {
         const query = 'INSERT INTO review (UserID, Comment) VALUES (?, ?)';
         db.query(query, [UserID,Comment], (err,result)=> {
             if (err) return callback(err);
             callback(null, result);
-        })
+        });
+    },
+
+    getReviewsByUserId: (UserID, callback) => {
+        const query = 'INSERT INTO review WHERE UserID = ?';
+        db.query(query, [UserID], (err,result)=> {
+            if (err) return callback(err);
+            callback(null, result);
+        });
     }
 }
+
+module.exports = Review;
