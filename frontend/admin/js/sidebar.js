77389872cb4e0efbb9bff227345591aea715a0e1
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-15 12:05:21 +0530

    refactor: update sidebar image and home CSS styles

--- frontend/admin/js/sidebar.js
+++ frontend/admin/js/sidebar.js
@@ -6,21 +6,32 @@ class SpecialSidebar extends HTMLElement {
                     <img src="../assets/sidebar-img/logo.png" alt="Logo" />
                 </div>
                 <div id="sidebar" class="bg-dark text-white text-center">
-                    <div class="sidebar-image">
-                        <img src="../assets/sidebar-img/man.avif" alt="User" />
+                    <div class="sidebar-header">
+                        <div class="sidebar-image">
+                            <img src="../assets/sidebar-img/man.jpg" alt="User" />
+                        </div>
+                        <div class="sidebar-title">
+                            <h4>Welcome</h4>
+                        </div>
                     </div>
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Home</a>
+                            <a class="nav-link text-white" href="#">Dashboard</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">About</a>
+                            <a class="nav-link text-white" href="#">Products</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Services</a>
+                            <a class="nav-link text-white" href="#">Orders</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Contact</a>
+                            <a class="nav-link text-white" href="#">Customers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Reviews</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Profile</a>
                         </li>
                     </ul>
                 </div>
