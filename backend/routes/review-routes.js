a0b2b27ee4e9c7e77c629ee7dd7711dd97b826d9
pawani mekhala <pawanimekhala3@gmail.com>
2024-10-18 12:52:43 +0530

    feat: add GET method for review route check

--- backend/routes/review-routes.js
+++ backend/routes/review-routes.js
@@ -7,7 +7,13 @@ const { authenticateUser } = require('../middleware/auth-middleware');
 // Route to add a review
 router.post('/add-review', authenticateUser, reviewController.addReview);
 
+router.get('/add-review', (req, res) => {
+    res.send("Review route is working");
+});
+
 // Route to fetch all reviews
 router.get('/get-reviews', reviewController.getAllReviews);
+
+
 
 module.exports = router;
