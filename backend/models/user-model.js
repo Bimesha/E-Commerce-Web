cc6ee8672da2dc5d38779722460445ca91913ae2
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-29 09:00:22 +0530

    refactor: add getCustomers function to fetch all customer details from the database

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -2,7 +2,8 @@ const db = require("../config/db");
 
 // Function to create a user
 const createUser = async ({ firstname, lastname, email, password }) => {
-  const query = "INSERT INTO user (FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?)";
+  const query =
+    "INSERT INTO user (FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?)";
   await db.execute(query, [firstname, lastname, email, password]);
 };
 
@@ -13,7 +14,27 @@ const findByEmail = async (email) => {
   return rows[0];
 };
 
+// Fetch all customer details from the database for admin page
+const getCustomers = async () => {
+  const query = `
+        SELECT u.UserID AS customerId, 
+               CONCAT(u.FirstName, ' ', u.LastName) AS name, 
+               u.Email AS email, 
+               u.Address AS address, 
+               u.Phone AS phone
+        FROM user u
+    `;
+  try {
+    const [rows] = await db.query(query);
+    return rows;
+  } catch (error) {
+    console.error("Error fetching customers:", error);
+    throw error;
+  }
+};
+
 module.exports = {
   createUser,
-  findByEmail,    
+  findByEmail,
+  getCustomers,
 };
