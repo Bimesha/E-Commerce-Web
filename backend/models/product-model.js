a7d42ede86b6c2099f9556156cb7b993b7ab4091
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-30 10:19:15 +0530

    refactor: update getProductById query to include productId in product-model.js

--- backend/models/product-model.js
+++ backend/models/product-model.js
@@ -3,7 +3,7 @@ const db = require("../config/db");
 // Fetch all products function from the database for admin page
 const getProducts = async () => {
   const query = `
-        SELECT p.Name AS name, p.Price AS price, p.Quantity AS quantity, p.ImagePath AS image,
+        SELECT  p.ProductID as productId, p.Name AS name, p.Price AS price, p.Quantity AS quantity, p.ImagePath AS image,
                c.CategoryName AS category
         FROM product p
         JOIN category c ON p.CategoryID = c.CategoryID
