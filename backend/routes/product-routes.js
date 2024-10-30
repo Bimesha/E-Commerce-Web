bb58536415a206ba562c4e09247829687f1ff420
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-30 09:42:23 +0530

    refactor: update product routes to use new controller functions

--- backend/routes/product-routes.js
+++ backend/routes/product-routes.js
@@ -1,8 +1,14 @@
-const express = require('express');
-const productsController = require('../controllers/product-controller');
+const express = require("express");
+const {
+  getAllProducts,
+  createNewProduct,
+  getProductsByCategory,
+  getProductDetails,
+} = require("../controllers/product-controller");
 const router = express.Router();
+const upload = require("../middleware/multer"); // for image upload
 
 // Route to get all products
-router.get('/all-products', productsController.getAllProducts);
+router.get("/all-products", getAllProducts);
 
 module.exports = router;
