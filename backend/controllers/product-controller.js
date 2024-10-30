5ca475405b93cf525ea9baec5378a3629293b2a4
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-30 09:34:34 +0530

    refactor: add createNewProduct function to product controller

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -1,5 +1,10 @@
-const db = require('../config/db'); 
-const { getProducts } = require('../models/product-model');
+const cloudinary = require("../config/cloudinary");
+const fs = require("fs");
+const {
+  createProduct,
+  getProducts,
+  getProductById,
+} = require("../models/product-model");
 
 // Controller to fetch all products to the admin page
 const getAllProducts = async (req, res) => {
@@ -7,11 +12,57 @@ const getAllProducts = async (req, res) => {
     const products = await getProducts();
     res.json(products);
   } catch (err) {
-    console.error('Error in getAllProducts controller:', err);
-    res.status(500).json({ error: 'Database error' });
+    console.error("Error in getAllProducts controller:", err);
+    res.status(500).json({ error: "Database error" });
+  }
+};
+
+// Controller to create a new product in the database
+const createNewProduct = async (req, res) => {
+  const { name, description, price, quantity, categoryID } = req.body;
+  const imageFile = req.file;
+
+  if (
+    !name ||
+    !description ||
+    !price ||
+    !quantity ||
+    !categoryID ||
+    !imageFile
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    // Upload image to Cloudinary
+    const uploadResult = await cloudinary.uploader.upload(imageFile.path, {
+      folder: "products",
+    });
+
+    // Get the image URL to store in the database
+    const imagePath = uploadResult.secure_url;
+
+    // Delete the image file from the temp folder after Cloudinary upload
+    fs.unlinkSync(imageFile.path);
+
+    const productData = {
+      name,
+      description,
+      price,
+      quantity,
+      categoryID,
+      imagePath,
+    };
+
+    await createProduct(productData);
+    res.status(201).json({ message: "Product created successfully" });
+  } catch (err) {
+    console.error("Error creating product:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 module.exports = {
-    getAllProducts
+  getAllProducts,
+  createNewProduct,
 };
