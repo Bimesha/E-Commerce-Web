9c70713e2c4e532e0f153ca6776abb896f207cfd
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-30 09:28:45 +0530

    refactor: add getProductById function to product model

--- backend/models/product-model.js
+++ backend/models/product-model.js
@@ -17,6 +17,25 @@ const getProducts = async () => {
   }
 };
 
+// model to get a product details by its ID
+const getProductById = async (productId) => {
+  const query = `
+    SELECT p.ProductID, p.Name, p.Description, p.Price, p.Quantity, p.CategoryID, p.ImagePath,
+           c.CategoryName AS category
+    FROM product p
+    JOIN category c ON p.CategoryID = c.CategoryID
+    WHERE p.ProductID = ?
+  `;
+  try {
+    const [rows] = await db.query(query, [productId]);
+    return rows[0]; // Return the first matching product or undefined
+  } catch (error) {
+    console.error("Error fetching product by ID:", error);
+    throw error;
+  }
+};
+
 module.exports = {
   getProducts,
+  getProductById,
 };
