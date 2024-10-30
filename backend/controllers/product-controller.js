15f096de50cb5a36789f3a857f8bb14a28194b36
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-30 09:36:05 +0530

    refactor: add getProductDetails function to product controller

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -62,7 +62,25 @@ const createNewProduct = async (req, res) => {
   }
 };
 
+// Controller to get a product details by its ID
+const getProductDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await getProductById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    console.error("Error fetching product by ID:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   getAllProducts,
   createNewProduct,
+  getProductDetails,
 };
