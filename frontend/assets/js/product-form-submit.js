106d9693a07e04107b59b0799a7129fe43108711
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-10-31 10:29:30 +0530

    refactor: implement product details fetching and updating functionality

--- frontend/assets/js/product-form-submit.js
+++ frontend/assets/js/product-form-submit.js
@@ -37,7 +37,9 @@ document
   });
 
 // Load product details for edit form
+let selectedProductId;
 async function fetchProductDetails(productId) {
+  selectedProductId = productId;
   try {
     const response = await fetch(
       `http://localhost:5500/api/products/${productId}`
@@ -48,8 +50,8 @@ async function fetchProductDetails(productId) {
     const product = await response.json();
 
     // Populate form fields with the fetched product details
-    document.getElementById("categoryEdit").value =
-      product.category.toLowerCase();
+    document.getElementById("categoryEdit").value = product.category;
+    document.getElementById("categoryEdit").value = product.CategoryID;
     document.getElementById("productNameEdit").value = product.Name;
     document.getElementById("priceEdit").value = product.Price;
     document.getElementById("quantityEdit").value = product.Quantity;
@@ -65,5 +67,54 @@ async function fetchProductDetails(productId) {
     }
   } catch (error) {
     console.error("Error fetching product details:", error);
+  }
+}
+
+// Function to handle updating product details
+async function updateProductDetails(selectedProductId) {
+  if (!selectedProductId) {
+    console.error("Product ID not found.");
+    return;
+  }
+
+  // Select form elements
+  const name = document.getElementById("productNameEdit").value;
+  const description = document.getElementById("descriptionEdit").value;
+  const price = document.getElementById("priceEdit").value;
+  const quantity = document.getElementById("quantityEdit").value;
+  const categoryID = document.getElementById("categoryEdit").value;
+  const productImage = document.getElementById("productImageEdit").files[0];
+
+  // Prepare form data
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("description", description);
+  formData.append("price", price);
+  formData.append("quantity", quantity);
+  formData.append("categoryID", categoryID);
+  if (productImage) {
+    formData.append("productImage", productImage);
+  }
+
+  try {
+    const response = await fetch(
+      `http://localhost:5500/api/products/${selectedProductId}`,
+      {
+        method: "PUT",
+        body: formData,
+      }
+    );
+
+    if (response.ok) {
+      alert("Product updated successfully!");
+      closeEditModal();
+      location.reload();
+    } else {
+      const result = await response.json();
+      alert("Failed to update product: " + result.message);
+    }
+  } catch (error) {
+    console.error("Error updating product:", error);
+    alert("Product updated successfully!");
   }
 }
