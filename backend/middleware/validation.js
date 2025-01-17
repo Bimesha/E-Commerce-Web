ad8d54163beda8658967bcbc731a25c50a37ecf1
PawaniMekhala <pawanimekhala3@gmail.com>
2025-01-17 14:36:33 +0530

    feat: add middleware to validate admin fields

--- backend/middleware/validation.js
+++ backend/middleware/validation.js
@@ -1,21 +1,31 @@
 const { body, validationResult } = require("express-validator");
 
+const passwordValidation = body("password")
+  .isLength({ min: 6 })
+  .withMessage("Password must be at least 6 characters long")
+  .matches(/[A-Z]/)
+  .withMessage("Password must contain at least one uppercase letter")
+  .matches(/[a-z]/)
+  .withMessage("Password must contain at least one lowercase letter")
+  .matches(/[0-9]/)
+  .withMessage("Password must contain at least one number")
+  .matches(/[!@#$%^&*(),.?":{}|<>]/)
+  .withMessage("Password must contain at least one special character");
+
 const validateUserCreation = [
-  body("firstname").notEmpty().withMessage("First name is required"),
-  body("lastname").notEmpty().withMessage("Last name is required"),
+  body("firstName").notEmpty().withMessage("First name is required"),
+  body("lastName").notEmpty().withMessage("Last name is required"),
   body("email").isEmail().withMessage("Valid email is required"),
-  body("password")
-      .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 characters long"),
+  passwordValidation,
   body("confirmPassword")
-      .notEmpty()
-      .withMessage("Confirm password is required")
-      .custom((value, { req }) => {
-        if (value !== req.body.password) {
-          throw new Error("Passwords do not match");
-        }
-        return true;
-      }),
+    .notEmpty()
+    .withMessage("Confirm password is required")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -36,7 +46,26 @@ const validateUserLogin = [
     next();
   },
 ];
-module.exports = { 
+
+// Middleware to validate admin fields
+const validateAdminCreation = [
+  body("firstName").notEmpty().withMessage("First name is required"),
+  body("lastName").notEmpty().withMessage("Last name is required"),
+  body("email").isEmail().withMessage("Valid email is required"),
+  passwordValidation,
+  body("address").notEmpty().withMessage("Address is required"),
+  body("phone").isMobilePhone().withMessage("A valid phone number is required"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+module.exports = {
   validateUserCreation,
   validateUserLogin,
- };
+  validateAdminCreation,
+};
