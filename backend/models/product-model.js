865dd14a3599615e215ec6ab49a91809631bdd20
PawaniMekhala <pawanimekhala3@gmail.com>
2025-01-17 14:40:33 +0530

    feat: add models to getAllProductsByCategory,  getPopularProducts, getNewProducts.

--- backend/models/product-model.js
+++ backend/models/product-model.js
@@ -1,10 +1,19 @@
 const db = require("../config/db");
 
-// Fetch all products function from the database for admin page
+// model to get all products from the database
 const getProducts = async () => {
   const query = `
-        SELECT  p.ProductID as productId, p.Name AS name, p.Price AS price, p.Quantity AS quantity, p.ImagePath AS image,
-               c.CategoryName AS category
+        SELECT  
+        p.ProductID as productId, 
+        p.Name AS name, 
+        p.Price AS price, 
+        p.Quantity AS quantity, 
+        p.ImagePath AS image,
+        c.CategoryName AS category,
+        CASE 
+        WHEN p.Quantity > 0 THEN 'In Stock'
+        ELSE 'Out of Stock'
+      END AS stockStatus
         FROM product p
         JOIN category c ON p.CategoryID = c.CategoryID
     `;
@@ -41,18 +50,47 @@ const createProduct = async (productData) => {
   }
 };
 
+// model to get products by category
+const getAllProductsByCategory = async (categoryId) => {
+  const query = `SELECT ProductID, Name, Price, Description,Quantity,ImagePath, 
+   CASE
+   WHEN Quantity > 0 THEN 'In Stock'
+   ELSE 'Out of Stock'
+   END AS StockStatus
+  FROM product 
+  WHERE CategoryID = ?`;
+  try {
+    const [result] = await db.query(query, [categoryId]);
+    return result;
+  } catch (error) {
+    console.error("Error getting all products by category:", error);
+    throw error;
+  }
+};
+
 // model to get a product details by its ID
 const getProductById = async (productId) => {
   const query = `
-    SELECT p.ProductID, p.Name, p.Description, p.Price, p.Quantity, p.CategoryID, p.ImagePath,
-           c.CategoryName AS category
+    SELECT 
+    p.ProductID, 
+    p.Name, 
+    p.Description, 
+    p.Price, 
+    p.Quantity, 
+    p.CategoryID, 
+    p.ImagePath,
+    c.CategoryName AS category,
+    CASE
+   WHEN Quantity > 0 THEN 'In Stock'
+   ELSE 'Out of Stock'
+   END AS StockStatus
     FROM product p
     JOIN category c ON p.CategoryID = c.CategoryID
     WHERE p.ProductID = ?
   `;
   try {
     const [rows] = await db.query(query, [productId]);
-    return rows[0]; // Return the first matching product or undefined
+    return rows[0];
   } catch (error) {
     console.error("Error fetching product by ID:", error);
     throw error;
@@ -101,10 +139,70 @@ const deleteProductById = async (productId) => {
   }
 };
 
+// model to get popular products
+const getPopularProducts = async () => {
+  const query = `
+  SELECT  
+        p.ProductID AS productId, 
+        p.Name AS name, 
+        p.Price AS price, 
+        p.Quantity AS quantity, 
+        p.ImagePath AS image,
+        c.CategoryName AS category,
+        SUM(oi.OIQuantity) AS totalSold,
+        CASE 
+          WHEN p.Quantity > 0 THEN 'In Stock'
+          ELSE 'Out of Stock'
+        END AS stockStatus
+        FROM product p
+        JOIN category c ON p.CategoryID = c.CategoryID
+        JOIN orderitem oi ON p.ProductID = oi.ProductID
+        GROUP BY p.ProductID
+        ORDER BY totalSold DESC
+        LIMIT 8;
+`;
+  try {
+    const [rows] = await db.query(query);
+    return rows;
+  } catch (error) {
+    console.error("Error fetching popular products:", error);
+    throw error;
+  }
+};
+
+// model to get newly added products
+const getNewProducts = async () => {
+  const query = `
+  SELECT  
+        p.ProductID AS productId, 
+        p.Name AS name, 
+        p.Price AS price, 
+        p.Quantity AS quantity, 
+        p.ImagePath AS image,
+        CASE 
+          WHEN p.Quantity > 0 THEN 'In Stock'
+          ELSE 'Out of Stock'
+        END AS stockStatus
+        FROM product p
+        ORDER BY ProductID DESC
+        LIMIT 8;
+`;
+  try {
+    const [rows] = await db.query(query);
+    return rows;
+  } catch (error) {
+    console.error("Error fetching new products:", error);
+    throw error;
+  }
+};
+
 module.exports = {
-  getProducts,
   createProduct,
+  getProducts,
+  getAllProductsByCategory,
   getProductById,
   updateProductById,
   deleteProductById,
+  getPopularProducts,
+  getNewProducts,
 };
